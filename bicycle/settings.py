2531af15827e7b9d7f0c5d8a7b53063601546355
jujuclubw <dlrkdwn428@gmail.com>
2023-06-14 15:04:36 +0900

    분석추가(개선필요)

--- bicycle/settings.py
+++ bicycle/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = "bicycle.wsgi.application"
 
 # 로그인 테스트를 진행하기 위함.
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': '1team_f',
-    #     'USER': 'root',
-    #     'PASSWORD': 'kor123',
-    #     'HOST': '61.74.225.3',
-    #     'PORT': '3306',
-    # }
-     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '1team_f',
+        'USER': 'root',
+        'PASSWORD': 'kor123',
+        'HOST': '61.74.225.3',
+        'PORT': '3306',
     }
+    #  "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     
 }
 
@@ -140,7 +140,7 @@ STATICFILES_DIRS =[
     os.path.join(BASE_DIR,"bicycleapp","static"),
     os.path.join(BASE_DIR,"ShowMap"),
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
