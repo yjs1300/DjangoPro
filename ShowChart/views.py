72d1b22758c08e72c56d4b6d0652d6aad4757cbc
Joon Gyu <system6805@naver.com>
2023-06-21 11:07:03 +0900

    로그인 기능 전체 수정했습니다.

--- ShowChart/views.py
+++ ShowChart/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import folium
+import pandas as pd
+import json
 
 # Create your views here.
 def index(request):
@@ -14,4 +17,69 @@ def card(request):
 def forgotpw(request):
     return render(request,"forgot-password.html")
 def charts(request):
-    return render(request,"charts.html")
+    
+    df = pd.read_excel("./rental3.xlsx")
+    # 지도의 기준이 되는 위도 경도 
+    # 서울의 위도 경도이다.
+    latitude = 37.562225
+    longitude = 126.978555
+    
+    # 지도 생성함
+    m  = folium.Map(
+        location= [latitude, longitude],
+        zoom_start=10, # 줌
+        tiles= "OpenStreetMap",
+    )
+    
+    with open('./hangjeongdong_.json',mode='rt',encoding='utf-8') as f:
+        geo = json.loads(f.read())    
+    
+    for feature in geo['features']:
+        code = feature['properties']['code']
+        name = feature['properties']['name']
+        print(code) 
+        print(name)
+        f.close()
+
+    folium.Choropleth(
+        geo_data= geo,
+        name='choropleth',
+        data=df, 
+        columns=['지역번호','대여소갯수'], # 데이터프레임의 컬럼들
+        key_on='feature.properties.code', # geojson 피처의 키
+        fill_color='YlGn',
+        fill_opacity=0.7,
+        line_opacity=0.7,
+        color = 'black',
+        overlay="True",
+        legend_name='대여소 분포',
+    ).add_to(m)
+    
+    maps1 = m._repr_html_()
+    
+        # 서울시 지자체별 파일
+    state_geo = 'https://raw.githubusercontent.com/southkorea/seoul-maps/master/kostat/2013/json/seoul_municipalities_geo_simple.json'
+    
+    # 서울시 유동인구 파일(행정구역별,나이대별,총이동)
+    df = pd.read_csv('./seoul_float_pop_age.csv',encoding='utf-8')
+    p=df[['행정구역별','총이동']]
+
+    # 지도 생성함    
+    elec_district_geo_map = folium.Map(location=[latitude, longitude], tiles="OpenStreetMap", zoom_start=10)
+    
+    # 시각화 
+    folium.Choropleth(
+        geo_data=state_geo,
+        name='자치구 별 유동인구',
+        data=p,
+        columns=('행정구역별','총이동'),
+        key_on='feature.properties.name',
+        fill_color='Greens',
+        fill_opacity = 0.7,
+        line_opacity = 0.7,
+        color = 'black',
+        legend_name = '유동인구 수'
+    ).add_to(elec_district_geo_map)
+
+    maps2 = elec_district_geo_map._repr_html_()
+    return render(request,"charts.html",{"maps1":maps1,"maps2":maps2})
